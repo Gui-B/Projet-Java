75ec0f04ae73ac6d93e0b381cfa29a436a320f7d
Boulic Guillaume <guillaume@boulic.fr>
2016-01-22 15:40:26 +0100

    ajout de la premiére génération de requette, pour la liste des utilisateur. elle est apellable avec ou sans paramettre ( surcharge )

--- java-connect/src/main/java/common/Protocole.java
+++ java-connect/src/main/java/common/Protocole.java
@@ -2,9 +2,27 @@ package common;
 
 public class Protocole {
 
-	private String 
+	private final String listUser = "LIST_USERS";
+	private final String detailUser = "DETAIL_USER";
+	private final String creerCompte = "CREER_COMPTE";
+	private final String modifInfo = "MODIF_INFO";
+	private final String ajoutDiplome = "AJOUT_DIPLOME";
+	private final String suppDiplome = "SUPP_DIPLOME";
+	private final String addComp = "AJOUT_COMP";
+	private final String delComp = "DEL_COMP";
+	private final String connection = "CONNECTION";
+	
 	public Protocole() {
-		
 	}
+	
+	public String reqListUsers(){
+		return this.reqListUsers(0);
+	}
+	
+	public String reqListUsers(int ID){
+		return this.listUser + "|" + ID;
+	}
+	
+	
 
 }

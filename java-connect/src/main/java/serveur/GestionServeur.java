c3e5789a074f252e66d7f088fc83c61846412ff5
Boulic Guillaume <guillaume@boulic.fr>
2016-01-24 17:49:53 +0100

    ajout de commentaire

--- java-connect/src/main/java/serveur/GestionServeur.java
+++ java-connect/src/main/java/serveur/GestionServeur.java
@@ -7,6 +7,11 @@ public class GestionServeur
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * fonction de traitement du message reçu par le serveur
+	 * @param message le message reçu par le serveur
+	 * @return la réponse que le serveur envoie au client
+	 */
 	public String traiter (String message)
 	{
 		return "lol";

df89dae9d16a3f671fb70d58e9968e781ab63d42
robin <robin@asus>
2016-01-25 23:24:08 +0100

    creer detruire ok

--- java-connect/src/main/java/sql/MySql.java
+++ java-connect/src/main/java/sql/MySql.java
@@ -2,11 +2,31 @@
  * 
  */
 package sql;
+import java.sql.*;
 
 /**
  * @author robin
  *
  */
-public class MySql {
+public class MySql 
+{
+	public static Connection connexion ()
+	{
+		String url="jdbc:mysql://kriissss.fr:3306/projJava";
+		String username="projJava";
+		String password="TNYVxtXuYAmvYeaC";
+		Connection db=null;
+		try 
+		{
+			db = DriverManager.getConnection(url, username, password);
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+
+		return db;
+
+	}
 
 }
--- projJava.sql
+++ projJava.sql
@@ -1,97 +1,58 @@
---
--- Base de données :  'projJava'
---
 
---
--- Nettoyage de la base;
---
+DROP TABLE IF EXISTS Obtenir;
+DROP TABLE IF EXISTS Avoir;
+DROP TABLE IF EXISTS Diplome;
+DROP TABLE IF EXISTS Competences;
+DROP TABLE IF EXISTS Utilisateur;
 
-DROP IF EXISTS TABLE Obtenir;
-DROP IF EXISTS TABLE Avoir;
-DROP IF EXISTS TABLE Diplome;
-DROP IF EXISTS TABLE Competences;
-DROP IF EXISTS TABLE Utilisateur;
-
--- --------------------------------------------------------
-
---
--- Structure de la table 'Utilisateur'
---
-
-CREATE TABLE IF NOT EXISTS 'Utilisateur' (
-  'IdU' int(11) NOT NULL,
-  'Mail' varchar(32) NOT NULL,
-  'Nom' varchar(32) NOT NULL,
-  'Prenom' varchar(32) NOT NULL,
-  'Admin' tinyint(1) NOT NULL
+CREATE TABLE IF NOT EXISTS Utilisateur (
+  IdU int(6) NOT NULL,
+  Mail varchar(128) NOT NULL,
+  Nom varchar(256) NOT NULL,
+  Prenom varchar(256) NOT NULL,
+  Admin tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-ALTER TABLE 'Utilisateur'
- ADD PRIMARY KEY ('IdU'), ADD UNIQUE KEY 'Mail' ('Mail');
+ALTER TABLE Utilisateur 
+  ADD CONSTRAINT PK_UTILISATEUR PRIMARY KEY (IdU),
+  ADD CONSTRAINT UK_UTILISATEUR UNIQUE KEY Mail (Mail);
 
--- --------------------------------------------------------
-
---
--- Structure de la table 'Competences'
---
-
-CREATE TABLE IF NOT EXISTS 'Competences' (
-  'idC' int(11) NOT NULL,
-  'NomC' varchar(32) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS Competences (
+  idC int(6) NOT NULL,
+  NomC varchar(256) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE 'Competences'
- ADD PRIMARY KEY ('idC');
+ALTER TABLE Competences
+ ADD CONSTRAINT PK_COMPETENCES PRIMARY KEY (idC);
 
--- --------------------------------------------------------
-
---
--- Structure de la table 'Diplome'
---
-
-CREATE TABLE IF NOT EXISTS 'Diplome' (
-  'idD' int(11) NOT NULL,
-  'NomD' varchar(32) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS Diplome (
+  idD int(6) NOT NULL,
+  NomD varchar(256) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE 'Diplome'
- ADD PRIMARY KEY ('idD');
+ALTER TABLE Diplome 
+  ADD CONSTRAINT PK_DIPLOME PRIMARY KEY (idD);
 
--- --------------------------------------------------------
-
---
--- Structure de la table 'Avoir'
---
-CREATE TABLE IF NOT EXISTS 'Avoir' 
+CREATE TABLE IF NOT EXISTS Avoir 
 (
-  'idU' int(11) NOT NULL,
-  'idC' int(11) NOT NULL
+  idU int(6) NOT NULL,
+  idC int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE 'Avoir'
-  ADD PRIMARY KEY ('IdU','idC'), ADD KEY 'FK_Avoir_idC' ('idC');
-
-ALTER TABLE 'Avoir'
-  ADD CONSTRAINT 'FK_Avoir_idC' FOREIGN KEY ('idC') REFERENCES 'Competences' ('idC'),
-  ADD CONSTRAINT 'FK_Avoir_IdU' FOREIGN KEY ('IdU') REFERENCES 'Utilisateur' ('IdU');
+ALTER TABLE Avoir
+  ADD CONSTRAINT PK_AVOIR PRIMARY KEY (IdU,idC), ADD KEY FK_Avoir_idC (idC),
+  ADD CONSTRAINT FK_Avoir_idC FOREIGN KEY (idC) REFERENCES Competences (idC),
+  ADD CONSTRAINT FK_Avoir_IdU FOREIGN KEY (IdU) REFERENCES Utilisateur (IdU);
 
 
--- --------------------------------------------------------
-
---
--- Structure de la table 'Obtenir'
---
-
-CREATE TABLE IF NOT EXISTS 'Obtenir' (
-  'DateD' date DEFAULT NULL,
-  'IdU' int(11) NOT NULL,
-  'idD' int(11) NOT NULL
+CREATE TABLE IF NOT EXISTS Obtenir (
+  DateD date DEFAULT NULL,
+  IdU int(6) NOT NULL,
+  idD int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE 'Obtenir'
- ADD PRIMARY KEY ('IdU','idD'), ADD KEY 'FK_Obtenir_idD' ('idD');
-
- ALTER TABLE 'Obtenir'
-  ADD CONSTRAINT 'FK_Obtenir_idD' FOREIGN KEY ('idD') REFERENCES 'Diplome' ('idD'),
-  ADD CONSTRAINT 'FK_Obtenir_IdU' FOREIGN KEY ('IdU') REFERENCES 'Utilisateur' ('IdU');
+ALTER TABLE Obtenir
+ ADD PRIMARY KEY (IdU,idD),
+ ADD CONSTRAINT FK_Obtenir_idD FOREIGN KEY (idD) REFERENCES Diplome (idD),
+ ADD CONSTRAINT FK_Obtenir_IdU FOREIGN KEY (IdU) REFERENCES Utilisateur (IdU);

e3501e9c39c36283320c7edac694a257fd6c76d6
Boulic Guillaume <guillaume@boulic.fr>
2016-01-25 23:50:44 +0100

    création de la classe diplome OK

--- java-connect/src/main/java/common/Diplome.java
+++ java-connect/src/main/java/common/Diplome.java
@@ -1,5 +1,35 @@
 package common;
 
 public class Diplome {
-
+	private String id;
+	private String diplome;
+	private String annee;
+	public Diplome(String id, String diplome, String annee) {
+		this.id = id;
+		this.diplome = diplome;
+		this.annee = annee;
+	}
+	public Diplome(String id, String diplome) {
+		this.id = id;
+		this.diplome = diplome;
+		this.annee ="0";
+	}
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @return the diplome
+	 */
+	public String getDiplome() {
+		return diplome;
+	}
+	/**
+	 * @return the annee
+	 */
+	public String getAnnee() {
+		return annee;
+	}
 }

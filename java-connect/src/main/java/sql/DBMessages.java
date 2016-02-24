969a3f0a0f06c8a0b02b1a1a6c485768c45f9ea5
robin <robin@asus>
2016-02-24 16:48:11 +0100

    inserer message ok

--- java-connect/src/main/java/sql/DBMessages.java
+++ java-connect/src/main/java/sql/DBMessages.java
@@ -1,24 +1,35 @@
 package sql;
 
+import java.security.Timestamp;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Time;
+import java.util.Date;
 
 import common.Competence;
+import common.Utilisateur;
 
 public class DBMessages {
 
-	public static boolean insererCompetence(Competence c)
+	public static boolean insererMessage(Utilisateur envoyeur, Utilisateur destinataire, String message)
 	{
 		try
 		{
+			//Date
+			Date date= new Date();
+			
 			// Connexion a la base de donnees avec la classe MySQL
 			// Insertion d'une competence requete SQL 
 			Connection db= MySql.connexion();
 			PreparedStatement pstmt = db.prepareStatement("INSERT INTO Messages (idEnvoyeur, idDestinataire, dateM, message) VALUES (?,?,?,?);");
 
 			// Parametres
-			pstmt.setString(1,c.getCompetence());
+			pstmt.setInt(1, envoyeur.getId());
+			pstmt.setInt(2, destinataire.getId());
+			pstmt.setLong(3, date.getTime());
+			pstmt.setString(4, message);
+			
 			
 			pstmt.executeUpdate();
 			db.close();
--- java-connect/src/main/java/sql/MySql.java
+++ java-connect/src/main/java/sql/MySql.java
@@ -76,7 +76,7 @@ public class MySql
 			db.createStatement().execute("ALTER TABLE Obtenir ADD PRIMARY KEY (IdU,idD), ADD CONSTRAINT FK_Obtenir_idD FOREIGN KEY (idD) REFERENCES Diplome (idD), ADD CONSTRAINT FK_Obtenir_IdU FOREIGN KEY (IdU) REFERENCES Utilisateur (IdU);");
 			
 			//Table Message
-			db.createStatement().execute("CREATE TABLE IF NOT EXISTS Messages(idM int(6) NOT NULL PRIMARY KEY AUTO_INCREMENT, idEnvoyeur int(6) NOT NULL, idDestinataire int(6) NOT NULL, dateM int(12) NOT NULL, message varchar(1024) NOT NULL, lu int(6) DEFAULT 0 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+			db.createStatement().execute("CREATE TABLE IF NOT EXISTS Messages(idM int(6) NOT NULL PRIMARY KEY AUTO_INCREMENT, idEnvoyeur int(6) NOT NULL, idDestinataire int(6) NOT NULL, dateM bigint NOT NULL, message varchar(1024) NOT NULL, lu int(6) DEFAULT 0 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
 			db.createStatement().execute("ALTER TABLE Messages ADD CONSTRAINT FK_messages_u1 FOREIGN KEY (idEnvoyeur) REFERENCES Utilisateur(IdU), ADD CONSTRAINT FK_messages_u2 FOREIGN KEY (IdDestinataire) REFERENCES Utilisateur(IdU);");
 			
 			

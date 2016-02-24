a5518c605692725e64a4bb5138a1c0d78673179c
Boulic Guillaume <boulic.guillaume@gmail.com>
2016-02-24 16:42:33 +0100

    création de nouvelle méthode

--- java-connect/src/main/java/client/GestionMessageriInstant.java
+++ java-connect/src/main/java/client/GestionMessageriInstant.java
@@ -15,5 +15,18 @@ public class GestionMessageriInstant {
 	public GestionMessageriInstant() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public String traiter (){
+		return "blbl";
+	}
+	
+	private String InitialisationSock(){
+	}
+	
+	private int StopSock(){
+	}
+	
+	
+	}
 
 }

ed6c47a60950d623014081141cf37c76b544fc53
Boulic Guillaume <boulic.guillaume@gmail.com>
2016-02-24 16:22:06 +0100

    création de la nouvelle classe

---
+++ java-connect/src/main/java/client/GestionMessageriInstant.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package client;
+
+/**
+ * @author kriss
+ *
+ */
+public class GestionMessageriInstant {
+
+	/**
+	 * 
+	 */
+	public GestionMessageriInstant() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

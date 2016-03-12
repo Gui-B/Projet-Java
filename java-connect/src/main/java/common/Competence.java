d4b1d73dba25ffbad9757669f4f9cbe20034b8d1
Boulic Guillaume <guillaume@boulic.fr>
2016-03-12 17:55:25 +0100

    add of the like counter in competence class

--- java-connect/src/main/java/common/Competence.java
+++ java-connect/src/main/java/common/Competence.java
@@ -3,11 +3,28 @@ package common;
 public class Competence {
 	private int id;
 	private String competence;
+	private int like;
+	
 	public Competence(int id, String competence) {
 		super();
 		this.id = id;
 		this.competence = competence;
+		this.like = 0;
 	}
+	
+	public Competence(int id, String competence, int like) {
+		super();
+		this.id = id;
+		this.competence = competence;
+		this.like = like;
+	}
+	/**
+	 * @return the like
+	 */
+	public int getLike() {
+		return like;
+	}
+
 	/**
 	 * @return the id
 	 */

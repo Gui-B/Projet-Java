b40938e9bf6b907f0405b20682c89185202880e6
robin <robin@asus>
2016-03-13 17:25:56 +0100

    get list conseilleurs ok

--- java-connect/src/main/java/sql/DBRecommander.java
+++ java-connect/src/main/java/sql/DBRecommander.java
@@ -2,14 +2,25 @@ package sql;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import common.Competence;
+import common.Message;
 import common.Recommander;
 import common.Utilisateur;
 
 public class DBRecommander extends MySql{
 	
+	/**
+	 * Ajoute la recommandation d'un utilisateur pour un autre.
+	 * @param conseilleur
+	 * @param recommande
+	 * @param competence
+	 * @return
+	 */
 	public static boolean recommander (Utilisateur conseilleur, Utilisateur recommande, Competence competence)
 	{
 		try
@@ -36,6 +47,13 @@ public class DBRecommander extends MySql{
 		
 	}
 	
+	/**
+	 * Supprime la recommandation d'un utilisateur pour un autre, en focntion d'une competence
+	 * @param conseilleur
+	 * @param recommande
+	 * @param competence
+	 * @return
+	 */
 	public static boolean supprimerRecommandation (Utilisateur conseilleur, Utilisateur recommande, Competence competence)
 	{
 		try
@@ -59,6 +77,49 @@ public class DBRecommander extends MySql{
 			return false;
 		}
 		
+	}
+	
+	/**
+	 * En fonction d'une compétence et d'un utilisateur, retourne la liste des utilisateurs qui le recommandent
+	 * @param recommande
+	 * @param competence
+	 * @return
+	 */
+	public static ArrayList<Utilisateur> getRecommandeurs (Utilisateur recommande, Competence competence)
+	{
+		ArrayList<Utilisateur> conseilleurs= new ArrayList<Utilisateur>();
+		
+		try
+		{
+			Connection db= connexion();
+			Statement s = null;
+		    ResultSet r = null; 
+			/* Création de l'objet gérant les requêtes */
+	        s = db.createStatement();
+	        
+	        /* Exécution d'une requête de lecture */
+	        PreparedStatement pstmt =db.prepareStatement( "SELECT idConseilleur FROM Recommander WHERE idRecommande=? AND idCompetence=? ");
+	        pstmt.setInt(1, recommande.getId());
+	        pstmt.setInt(2, competence.getId());
+	        
+	        r=pstmt.executeQuery();
+	        
+	        /* Récupération des données du résultat de la requête de lecture */
+	        while ( r.next() ) 
+	        {
+	        	Utilisateur conseilleur= DBUtilisateur.lireUtilisateur(r.getInt("idConseilleur"));
+	        	conseilleurs.add(conseilleur);
+	        	
+	        }
+	        r.close();
+	        s.close();
+	        db.close();
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return conseilleurs;
 	}
 	
 	
